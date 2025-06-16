6790eab5d6373fd4d1c01a7f88803bd25cc154cc
bahodir <vbahodir00@gmail.com>
2025-06-16 11:49:06 +0500

    agroconnect.com.uz whitelist v3

--- regions/models.py
+++ regions/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 from common.models import BaseModel
 
-class Region(BaseModel):
-    name = models.CharField(max_length=255)
-    country = models.CharField(max_length=255, default="Uzbekistan")
-
-    class Meta:
-        db_table = "Regions"
+# class Region(BaseModel):
+#     name = models.CharField(max_length=255)
+#     country = models.CharField(max_length=255, default="Uzbekistan")
+#
+#     class Meta:
+#         db_table = "Regions"
