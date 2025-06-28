3a3127318f245cace2661c065e11140a2086df09
bahodir <vbahodir00@gmail.com>
2025-06-28 16:36:20 +0500

    temp v1

--- products/models.py
+++ products/models.py
@@ -15,16 +15,16 @@ class Product(BaseModel):
 
 
 class PlantedProduct(BaseModel):
-    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True, blank=True,
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, null=True, blank=True,
                                 related_name='planted_products')
-    owner = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True, related_name='planted_products')
-    region = models.ForeignKey(Region, on_delete=models.SET_NULL, null=True, blank=True,
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True, related_name='planted_products')
+    region = models.ForeignKey(Region, on_delete=models.CASCADE, null=True, blank=True,
                                related_name='planted_products')
     planting_area = models.DecimalField(max_digits=15, decimal_places=3)
     expecting_weight = models.DecimalField(max_digits=15, decimal_places=3)
 
     class Meta:
         db_table = "Planted Products"
-
-    def __str__(self):
-        return f"Product name: {self.product.name}, Product owner: {self.owner.first_name}, Region: {self.region.name}"
+    #
+    # def __str__(self):
+    #     return f"Product name: {self.product.name}, Product owner: {self.owner.first_name}, Region: {self.region.name}"
